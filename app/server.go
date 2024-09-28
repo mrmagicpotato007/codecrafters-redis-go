196a509664b5f5b436b0330b605d4cd3e44ee273
magicpotato <mr.magicpotato007@gmail.com~>
2024-09-28 10:08:50 +0530

    [any message]

--- app/server.go
+++ app/server.go
@@ -6,24 +6,29 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	buff := make([]byte, 128)
+	_, err = conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error while reading from the connection: ", err.Error())
+	}
+	conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error while writing to the connection: ", err.Error())
 	}
 }
