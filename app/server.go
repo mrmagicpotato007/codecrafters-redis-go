972b67700e60c41856ec3d7de79abc1aead9e726
magicpotato <mr.magicpotato007@gmail.com~>
2024-11-24 16:40:48 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"strconv"
+	"strings"
 
 	"log"
 
 	"net"
 
 	"os"
+)
+
+const (
+	CLRF = "\r\n"
 )
 
 func main() {
@@ -44,11 +51,15 @@ func main() {
 
 func handleRequest(conn net.Conn) {
 
-	buff := make([]byte, 1024)
+	buff := make([]byte, 2048)
 
 	for {
+
 		_, err := conn.Read(buff)
 
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
 
 			fmt.Printf("Error reading: %#v\n", err)
@@ -56,9 +67,36 @@ func handleRequest(conn net.Conn) {
 			return
 
 		}
-
 		log.Println("raw data", string(buff))
-		conn.Write([]byte("+PONG\r\n"))
+		parseIp(string(buff), conn)
 	}
 
+}
+
+func parseIp(ip string, conn net.Conn) {
+
+	ipLength := len(ip)
+
+	if ipLength == 0 {
+		log.Println("invalid input")
+	}
+
+	switch ip[0] {
+	//we recieved array
+	//*2 \r\n $4 \r\n ECHO \r\n $3 \r\n hey \r\n
+	case '*':
+		array := strings.Split(ip, CLRF)
+		log.Println("first ele", array[0])
+		noOfElememts, err := strconv.Atoi(string(array[0][1]))
+		log.Println("no of elements", noOfElememts)
+		if err != nil {
+			fmt.Println("Error converting to integer:", err)
+		}
+		if array[2] == "ECHO" {
+			//$3\r\nhey\r\n
+			conn.Write([]byte(array[3] + CLRF + array[4] + CLRF))
+		} else {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
