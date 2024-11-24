6ff87437ff7de114e24ca04f855d671c451fb8da
magicpotato <mr.magicpotato007@gmail.com~>
2024-11-24 15:27:52 +0530

    [any message]

--- app/server.go
+++ app/server.go
@@ -8,10 +8,6 @@ import (
 	"net"
 
 	"os"
-
-	"strconv"
-
-	"strings"
 )
 
 func main() {
@@ -62,48 +58,6 @@ func handleRequest(conn net.Conn) {
 
 	rawData := string(buff[:length])
 	log.Println("raw data", rawData)
-	lines := strings.Split(rawData, "\n")
-
-	// if the received data is a array
-
-	if len(lines) > 0 && strings.HasPrefix(lines[0], "*") {
-
-		elements := []string{}
-
-		for i := 1; i < len(lines); i++ {
-
-			if strings.HasPrefix(lines[i], "$") {
-
-				elementLength, err := strconv.Atoi(strings.Trim(lines[i][1:], "\r"))
-
-				if err != nil {
-
-					log.Println("Error parsing element length:", err)
-
-					return
-
-				}
-
-				if i+1 < len(lines) && len(strings.Trim(lines[i+1], "\r")) == elementLength {
-
-					elements = append(elements, strings.Trim(lines[i+1], "\r"))
-
-					i++ // Skip the next line as it is part of the current element
-
-				}
-
-			}
-
-		}
-
-		if len(elements) == 1 && elements[0] == "PING" {
-
-			conn.Write([]byte("+PONG\r\n"))
-
-			return
-
-		}
-
-	}
+	conn.Write([]byte("PONG\r\n+PONG\r\n"))
 
 }
