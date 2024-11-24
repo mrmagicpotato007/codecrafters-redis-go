ea3d34978a12dc3db400565a4947c7018905767a
magicpotato <mr.magicpotato007@gmail.com~>
2024-11-24 17:09:20 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"strconv"
 	"strings"
+	"sync"
 
 	"log"
 
@@ -16,6 +17,20 @@ import (
 const (
 	CLRF = "\r\n"
 )
+
+var kv *KVStore
+
+type KVStore struct {
+	mu    sync.Mutex
+	store map[string]string
+}
+
+func NewKvStore() *KVStore {
+	return &KVStore{store: map[string]string{}}
+}
+func init() {
+	kv = NewKvStore()
+}
 
 func main() {
 
@@ -67,7 +82,8 @@ func handleRequest(conn net.Conn) {
 			return
 
 		}
-		log.Println("raw data", string(buff))
+		data := strings.ReplaceAll(string(buff), "\n", `\n`)
+		log.Println("raw data", data)
 		parseIp(string(buff), conn)
 	}
 
@@ -83,20 +99,53 @@ func parseIp(ip string, conn net.Conn) {
 
 	switch ip[0] {
 	//we recieved array
-	//*2 \r\n $4 \r\n ECHO \r\n $3 \r\n hey \r\n
+
 	case '*':
 		array := strings.Split(ip, CLRF)
-		log.Println("first ele", array[0])
+
 		noOfElememts, err := strconv.Atoi(string(array[0][1]))
 		log.Println("no of elements", noOfElememts)
 		if err != nil {
 			fmt.Println("Error converting to integer:", err)
 		}
 		if array[2] == "ECHO" {
-			//$3\r\nhey\r\n
+			//ip://*2 \r\n $4 \r\n ECHO \r\n $3 \r\n hey \r\n
+			//op://$3\r\nhey\r\n
 			conn.Write([]byte(array[3] + CLRF + array[4] + CLRF))
+		} else if array[2] == "SET" {
+			//ip://"*3 \r\n $3 \r\n SET \r\n $6 \r\n orange \r\n $5 \r\n apple \r\n
+			//op://+OK\r\n
+			store(array[4], array[6])
+			log.Printf("received key %s val %s \n", array[4], array[6])
+			conn.Write([]byte("+OK\r\n"))
+		} else if array[2] == "GET" {
+			//ip://*2 \r\n $3 \r\n GET \r\n $9 \r\n pineapple \r\n
+			val, err := get(array[4])
+			if err != nil {
+				conn.Write([]byte("$-1\r\n"))
+				return
+			}
+			conn.Write([]byte("$" + strconv.Itoa(len(val)) + CLRF + val + CLRF))
+
 		} else {
 			conn.Write([]byte("+PONG\r\n"))
 		}
 	}
+}
+
+func store(key string, val string) error {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[key] = val
+	log.Println("stored succesfully")
+	return nil
+}
+
+func get(key string) (string, error) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if kv.store[key] != "" {
+		return kv.store[key], nil
+	}
+	return "", fmt.Errorf("key not found in kv store")
 }
